8202e200462f5059231c9dc5f5399ea9cca16a31
juanitomiller <juancristobalcamposferrada@gmail.com>
2025-02-23 01:28:51 -0300

    Upload files

---
+++ data/products.js
@@ -0,0 +1,158 @@
+const products = [
+    {
+        id: 1,
+        name: 'Coors Botellon 330cc',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=2509&field=image_1024',
+        price: '4900',
+        originalPrice: '12990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 2,
+        name: 'Austral Calafate Patagonia',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=13362&field=image_1024',
+        price: '9990',
+        originalPrice: '13990',
+        buttonText: 'Añadir',
+        sale: false,
+        rating: 5,
+    },
+    {
+        id: 3,
+        name: 'Heineken 350cc ',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=2511&field=image_1024',
+        price: '12990',
+        originalPrice: '17990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 4,
+        name: 'Cerveza original SOL',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=2525&field=image_1024',
+        price: '12990',
+        originalPrice: '15990',
+        buttonText: 'Añadir',
+        sale: false,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 5,
+        name: 'Royal Guard American Pale Ale',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=13794&field=image_1024',
+        price: '13990',
+        originalPrice: '18990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 6,
+        name: 'Heineken Silver Lager',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=11610&field=image_1024',
+        price: '11990',
+        originalPrice: '17990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 7,
+        name: 'Coors Original',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=4464&field=image_1024',
+        price: '9990',
+        originalPrice: '12990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+    {
+        id: 8,
+        name: 'Pataginia 470cc Variedades',
+        image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=16472&field=image_1024',
+        price: '17990',
+        originalPrice: '19990',
+        buttonText: 'Añadir',
+        sale: true,
+        rating: 5,
+        description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+
+    },
+
+    {
+    
+           id: 9,
+           name: '6x Vaso Schopero La Barra 570cc',
+           image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=11803&field=image_1024',
+           price: '17990',
+           originalPrice: '19990',
+           buttonText: 'Añadir',
+           sale: true,
+           rating: 5,
+           description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+   
+       }
+       ,
+
+    {
+    
+           id: 10,
+           name: '6x Copa Cocktail La Barra 410cc',
+           image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=11806&field=image_1024',
+           price: '17990',
+           originalPrice: '19990',
+           buttonText: 'Añadir',
+           sale: true,
+           rating: 5,
+           description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+   
+       }
+       ,
+
+    {
+    
+           id: 11,
+           name: '6x Copa Ramazzotti 490cc',
+           image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=2586&field=image_1024',
+           price: '17990',
+           originalPrice: '19990',
+           buttonText: 'Añadir',
+           sale: true,
+           rating: 5,
+           description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+   
+       }
+       ,
+
+    {
+    
+           id: 12,
+           name: '6x Copa Ramazzotti Violetto 550cc',
+           image: 'https://labarraodoo.cloudccu.cl/web/image?model=product.product&id=12104&field=image_1024',
+           price: '17990',
+           originalPrice: '19990',
+           buttonText: 'Añadir',
+           sale: true,
+           rating: 5,
+           description: "Diseñado pensando en la comodidad y funcionalidad, este producto combina elegancia y practicidad. Sus características innovadoras lo convierten en una opción superior para quienes buscan calidad y estilo."
+   
+       }
+    
+];
+
+export default products;
